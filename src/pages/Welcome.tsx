3748d6407d0cfd95b069a015c2becc11d1d1f233
Ricardo Pabón <rpabon257@gmail.com>
2020-12-21 15:39:50 +0100

    Fix welcome screen

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -5,8 +5,14 @@ export function Welcome() {
   const history = useHistory();
 
   useEffect(() => {
-    setTimeout(() => void history.push('/sessions'), 2000);
+    setTimeout(() => void history.push('/sessions'), 3500);
   }, [history]);
 
-  return <div>Welcome</div>;
+  return (
+    <div className="bg-primary-default opacity-60">
+      <div className="container h-screen flex items-center justify-center">
+        <h1 className="text-white font-bold text-4xl">Create Sessions</h1>
+      </div>
+    </div>
+  );
 }
